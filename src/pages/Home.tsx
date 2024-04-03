ce00ebbe21d20e698e6071811aa4a40d4acabd6e
vinhbv <vinhhd94@gmail.com>
2024-04-03 11:43:54 +0700

    update: update reward

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,9 @@ export default function Home() {
   const revolutionsStep1 = 1;
   const revolutionsStep2 = 4;
   const revolutionsStep3 = 2;
+  const timeStep1 = 2000;
+  const timeStep2 = 4000;
+  const timeStep3 = 3000;
   const spinTurnRef = useRef(revolutionsStep1);
 
   const size = 140;
@@ -33,8 +36,7 @@ export default function Home() {
   };
 
   const [isSpinning, setIsSpinning] = useState(false);
-
-  console.log("isSpinning", isSpinning);
+  const [rewardId, setRewardId] = useState<number>();
 
   const onStart = async () => {
     if (spinRef.current) {
@@ -43,48 +45,50 @@ export default function Home() {
         console.log("spinTurnRef.current", spinTurnRef.current);
         if (spinRef.current) {
           spinRef.current.style.transitionTimingFunction = "ease-in";
-          spinRef.current.style.transitionDuration = "2000ms";
+          spinRef.current.style.transitionDuration = `${timeStep1}ms`;
           spinRef.current.style.transform = `rotate(${
             360 * spinTurnRef.current // init = 2
           }deg)`;
-          await sleep(2000);
+          await sleep(timeStep1);
           spinRef.current.style.transitionTimingFunction = "linear";
-          spinRef.current.style.transitionDuration = "4000ms";
+          spinRef.current.style.transitionDuration = `${timeStep2}ms`;
           spinRef.current.style.transform = `rotate(${
             360 * (spinTurnRef.current + revolutionsStep2)
           }deg)`;
         }
       };
 
-      Promise.all([sleep(6000), fakeCallApi(), runningStep1AndStep2()]).then(
-        async (res) => {
-          console.log("xong lan 2");
-          const rewardId = res[1];
-          console.log("rewardId", rewardId);
+      Promise.all([
+        sleep(timeStep1 + timeStep2),
+        fakeCallApi(),
+        runningStep1AndStep2(),
+      ]).then(async (res) => {
+        console.log("xong lan 2");
+        const rewardId = res[1];
+        console.log("rewardId", rewardId);
 
-          const rewardDeg = rewardId * deg; // 0 60 120 180 240 300 360
+        const rewardDeg = rewardId * deg; // 0 60 120 180 240 300 360
 
-          if (spinRef.current) {
-            spinRef.current.style.transitionTimingFunction = "ease-out";
-            spinRef.current.style.transitionDuration = "4000ms";
-            spinRef.current.style.transform = `rotate(${
-              360 *
-                (spinTurnRef.current + revolutionsStep2 + revolutionsStep3) -
-              rewardDeg +
-              getRandomPositionInOneCone(deg)
-            }deg)`;
+        if (spinRef.current) {
+          spinRef.current.style.transitionTimingFunction = "ease-out";
+          spinRef.current.style.transitionDuration = `${timeStep3}ms`;
+          spinRef.current.style.transform = `rotate(${
+            360 * (spinTurnRef.current + revolutionsStep2 + revolutionsStep3) -
+            rewardDeg +
+            getRandomPositionInOneCone(deg)
+          }deg)`;
 
-            const totalRevolutions =
-              revolutionsStep1 + revolutionsStep2 + revolutionsStep3;
+          const totalRevolutions =
+            revolutionsStep1 + revolutionsStep2 + revolutionsStep3;
 
-            spinTurnRef.current += totalRevolutions;
-            console.log("xong");
-            await sleep(4000);
+          spinTurnRef.current += totalRevolutions;
+          console.log("xong");
+          await sleep(timeStep3);
 
-            setIsSpinning(false);
-          }
+          setIsSpinning(false);
+          setRewardId(rewardId);
         }
-      );
+      });
     }
   };
 
@@ -99,6 +103,10 @@ export default function Home() {
         onStart={onStart}
         spinRef={spinRef}
       />
+
+      <div className="text-white mt-4 flex gap-1">
+        Reward: {rewardId && <div> {rewardId + 1}$</div>}
+      </div>
     </div>
   );
 }
